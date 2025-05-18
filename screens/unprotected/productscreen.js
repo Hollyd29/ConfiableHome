4ec33d3c88820a4f5264a8c5c2a383081d830b65
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-05-18 05:58:54 +0100

    filter options completed

--- screens/unprotected/productscreen.js
+++ screens/unprotected/productscreen.js
@@ -1,8 +1,11 @@
 import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Button from "./component/button";
+import { useState } from "react";
 
 function ProductScreen() {
+  const [isShow, setIsShow] = useState(false);
+
   return (
     <View>
       <View style={styles.headerCon}>
@@ -14,12 +17,35 @@ function ProductScreen() {
             btnText={styles.searchText}
           />
         </View>
-        <Pressable>
+        <Pressable onPress={() => setIsShow(!isShow)}>
           <Text style={styles.filter}>
             <AntDesign name="filter" size={24} color="black" /> Filter
           </Text>
         </Pressable>
       </View>
+      {isShow && (
+        <View style={styles.filterOptCon}>
+          <View>
+            <Text style={styles.CatCom}>Category</Text>
+            <Text style={styles.categoryList}>All</Text>
+            <Text style={styles.categoryList}>Office</Text>
+            <Text style={styles.categoryList}>Living Room</Text>
+            <Text style={styles.categoryList}>Kitchen</Text>
+            <Text style={styles.categoryList}>Bedroom</Text>
+            <Text style={styles.categoryList}>Dining Room</Text>
+            <Text style={styles.categoryList}>Kids</Text>
+            <Button
+              title="Clear Filters"
+              btnStyle={styles.clearFilter}
+              btnText={styles.clearText}
+            />
+          </View>
+          <View>
+            <Text style={styles.CatCom}>Company</Text>
+            <TextInput value="All" style={styles.allCompany} />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -65,5 +91,47 @@ const styles = StyleSheet.create({
     color: "#ffff",
     fontSize: 20,
     fontWeight: 500,
+  },
+  filterOptCon: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline",
+    justifyContent: "space-between",
+    paddingTop: 20,
+    paddingHorizontal: 40,
+    backgroundColor: "#bbdefb",
+  },
+  CatCom: {
+    fontSize: 25,
+    fontWeight: "600",
+  },
+  categoryList: {
+    fontSize: 20,
+    fontWeight: "700",
+    opacity: 0.6,
+    marginTop: 20,
+  },
+  clearFilter: {
+    backgroundColor: "#ef233c",
+    padding: 10,
+    width: 150,
+    // alignSelf: "center",
+    borderRadius: 7,
+    marginBottom: 50,
+    marginTop: 20,
+  },
+  clearText: {
+    textAlign: "center",
+    color: "#ffff",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  allCompany: {
+    borderWidth: 1,
+    borderColor: "#8d99ae",
+    borderRadius: 7,
+    height: 40,
+    backgroundColor: "#fff",
+    marginTop: 10,
   },
 });
