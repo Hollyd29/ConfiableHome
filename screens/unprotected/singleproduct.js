161ad2eb2ade5e96e6a969f978245712d862fb4b
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-05-20 22:13:25 +0100

    single product setup

--- screens/unprotected/singleproduct.js
+++ screens/unprotected/singleproduct.js
@@ -1,10 +1,48 @@
-import React from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+import { url } from "../utils/urlstorage";
+import axios from "axios";
+import LoadingIcon from "../utils/loadingicon";
 
 function SingleProduct() {
+  const [singleProduct, setSingleProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigation = useNavigation();
+
+  const route = useRoute();
+  const id = route.params.productId;
+  //   console.log(id);
+
+  async function getSingleProduct() {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(`${url}/products/${id}`);
+      setSingleProduct(res.data.products);
+      //   console.log(res.data.products);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      console.log("nothing worked yet");
+
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getSingleProduct();
+  }, [id]);
 
   return (
     <View>
@@ -14,6 +52,17 @@ function SingleProduct() {
         </Pressable>
         <Text style={styles.headerText}>Details</Text>
       </View>
+      {isLoading ? (
+        <LoadingIcon />
+      ) : (
+        <ScrollView>
+          <View style={{ padding: 20 }}>
+            <Image source={{ uri: singleProduct.image }} style={styles.img} />
+            <Text></Text>
+            <Text></Text>
+          </View>
+        </ScrollView>
+      )}
     </View>
   );
 }
@@ -31,5 +80,10 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontWeight: "700",
+  },
+  img: {
+    height: 200,
+    width: "100%",
+    borderRadius: 10,
   },
 });
